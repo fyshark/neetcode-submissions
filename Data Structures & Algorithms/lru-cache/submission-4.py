7e0f80986c2af3a5ebe06ec37354c30dd7a4baf1
Yu Feng <fengyu@tcd.ie>
2026-06-01 15:46:40 +0200

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,54 @@
+class Node:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.oldest = Node(0, 0)
+        self.latest = Node(0, 0)
+        self.oldest.next = self.latest
+        self.latest.prev = self.oldest
+    
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def insert(self, node):
+        prev = self.latest.prev
+        nxt = self.latest
+        prev.next = node
+        node.prev = prev
+        nxt.prev = node
+        node.next = nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key].value = value
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return
+        node = Node(key, value)
+        self.insert(node)
+        self.cache[key] = node
+        
+        if len(self.cache) > self.capacity:
+            lru = self.oldest.next
+            self.remove(lru)
+            self.cache.pop(lru.key)
+        
