86eab0083ca8463d07562bcfad36ff8a039fa6fb
Yu Feng <fengyu@tcd.ie>
2026-08-03 15:37:41 +0200

    Add: combination-target-sum - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, target, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, int remain, List<Integer> temp, List<List<Integer>> res) {
+        if (remain < 0) {
+            return;
+        }
+
+        if (remain == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i=start; i<nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(nums, i, remain-nums[i], temp, res);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
