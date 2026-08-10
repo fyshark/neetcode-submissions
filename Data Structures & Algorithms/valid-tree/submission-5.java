feb670ff46795a10af51bb2acdf2eefaeda70149
Yu Feng <fengyu@tcd.ie>
2026-08-10 23:52:02 +0200

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge: edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        boolean[] visited = new boolean[n];
+
+        if (hasCycle(graph, 0, visited, -1)) return false;
+        for (int i=0; i<n; i++) {
+            if (!visited[i]) return false;
+        }
+        return true;
+    }
+
+    boolean hasCycle(List<List<Integer>> graph, int curr, boolean[] visited, int parent) {
+        visited[curr] = true;
+
+        for (int neighbor: graph.get(curr)) {
+            if ((visited[neighbor] && neighbor != parent) ||
+                (!visited[neighbor] && hasCycle(graph, neighbor, visited, curr))) {
+                    return true;
+            }
+        }
+        return false;
+    }
+}
