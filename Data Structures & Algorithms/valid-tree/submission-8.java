ac9a5e1ae921145d297591ad2d89aebc34f2420d
Yu Feng <fengyu@tcd.ie>
2026-08-11 22:35:11 +0200

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.java
@@ -0,0 +1,34 @@
+class Solution {
+    int[] parent;
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n-1) {
+            return false;
+        }
+
+        parent = new int[n];
+
+        for (int i=0; i<n; i++) {
+            parent[i] = i;
+        }
+
+        for (int[] edge: edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int rootU = find(u);
+            int rootV = find(v);
+
+            if (rootU == rootV) {
+                return false;
+            }
+            parent[rootV] = rootU;
+        }
+        return true;
+    }
+
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+}
