af4d388428b9d75a92f3d8d853778b114b9a9f68
Yu Feng <fengyu@tcd.ie>
2026-08-19 22:11:50 +0200

    Add: validate-parentheses - submission-19

---
+++ Data Structures & Algorithms/validate-parentheses/submission-19.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {']':'[', ')':'(', '}':'{'}
+        stack = []
+
+        for c in s:
+            if c in brackets.values():
+                stack.append(c)
+            elif not stack or stack.pop() != brackets[c]:
+                return False
+        
+        return not stack
